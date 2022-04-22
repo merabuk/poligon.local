3eab4f081d474d3340117718ea24eb2d08b6b0b7
Andrew <merabuk@gmail.com>
2022-04-22 21:27:32 +0300

    lesson 16

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -49,7 +49,11 @@ class CategoryController extends BaseController
      */
     public function edit($id)
     {
-        dd(__METHOD__);
+        $item = BlogCategory::findOrFail($id);
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.category.edit',
+            compact('item', 'categoryList'));
     }
 
     /**
@@ -61,6 +65,6 @@ class CategoryController extends BaseController
      */
     public function update(Request $request, $id)
     {
-        dd(__METHOD__);
+        dd(__METHOD__, $request->all(), $id);
     }
 }
