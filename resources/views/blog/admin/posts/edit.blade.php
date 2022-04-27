b24cdb49576a0cbff3a09eda5d3fd1f5423ea741
Andrew <merabuk@gmail.com>
2022-04-28 01:12:33 +0300

    lesson 39

--- resources/views/blog/admin/posts/edit.blade.php
+++ resources/views/blog/admin/posts/edit.blade.php
@@ -23,5 +23,22 @@
                 </div>
             </div>
         </form>
+        @if($item->exists)
+            <br/>
+            <form method="POST" action="{{ route('blog.admin.posts.destroy', $item->id) }}">
+                @method('DELETE')
+                @csrf
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card card-block">
+                            <div class="card-body ml-auto">
+                                <button type="submit" class="btn btn-link">Удалить</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+            </form>
+        @endif
     </div>
 @endsection
